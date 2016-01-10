4a3332e9d36112b508df0cefb87b7845d9ae9496
Janey <janey.keig@gmail.com>
2016-01-09 21:54:39 -0800

    .env ignore

--- server/routes/externalApis.js
+++ server/routes/externalApis.js
@@ -5,7 +5,7 @@ db = require('../models');
 
 
 router.get('/', function(req,res){
-  request('http://www.google.com', function (error, response, body) {
+  request('https://api.meetup.com/2/open_events', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body); // Show the HTML for the Google homepage. 
   }
