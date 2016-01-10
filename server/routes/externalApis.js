7a9306799f6921342d0b8af88211a199ac741d42
Janey <janey.keig@gmail.com>
2016-01-10 09:59:10 -0800

    working api call to meetup

--- server/routes/externalApis.js
+++ server/routes/externalApis.js
@@ -5,12 +5,13 @@ db = require('../models');
 
 
 router.get('/', function(req,res){
-  console.log(process.env.MEETUP_KEY);
-  // request('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUPKEY, function (error, response, body) {
-  // if (!error && response.statusCode == 200) {
-  //   console.log(body); // Show the HTML for the Google homepage. 
-  // }
-  // });
+  // console.log(process.env.MEETUP_KEY);
+  // console.log('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUP_KEY);
+  request('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUP_KEY, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body); // Testing
+  }
+  });
 });
 
 
