37f4a9acb86d4caa7b13011ee9d96a8971bfc8f7
Janey <janey.keig@gmail.com>
2016-01-09 22:21:16 -0800

    meetup endpoint

--- server/routes/externalApis.js
+++ server/routes/externalApis.js
@@ -5,7 +5,7 @@ db = require('../models');
 
 
 router.get('/', function(req,res){
-  request('https://api.meetup.com/2/open_events', function (error, response, body) {
+  request('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUPKEY, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body); // Show the HTML for the Google homepage. 
   }
