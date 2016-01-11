d8b6697da19fd4d8871a13276ecc4cfc56b3d7df
Janey <janey.keig@gmail.com>
2016-01-11 14:10:05 -0800

    added eventbrite endpoints

--- server/routes/externalApis.js
+++ server/routes/externalApis.js
@@ -4,7 +4,7 @@ router = express.Router(),
 db = require('../models');
 
 
-router.get('/', function(req,res){
+router.get('/meetup', function(req,res){
   // console.log(process.env.MEETUP_KEY);
   // console.log('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUP_KEY);
   request('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUP_KEY, function (error, response, body) {
@@ -15,7 +15,16 @@ router.get('/', function(req,res){
 });
 
 
+router.get('/eventbrite', function(req,res){
+  request('https://www.eventbriteapi.com/v3/events/search/?categories=110&venue.city=san-francisco&token='+ process.env.EVENTBRITE_KEY, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body); // Testing
+  }
+  });
+});
 
+//GRAB VENUE FOR EVENTBRITE 
+//https://www.eventbriteapi.com/v3/venues/12155401/?token=
 
 
 
