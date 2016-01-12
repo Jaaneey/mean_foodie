b25779ee0a46ba2106aecd1e5c677b65403dd5e9
Janey <janey.keig@gmail.com>
2016-01-11 17:32:44 -0800

    create meetup obj

--- server/routes/externalApis.js
+++ server/routes/externalApis.js
@@ -9,7 +9,22 @@ router.get('/meetup', function(req,res){
   // console.log('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUP_KEY);
   request('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUP_KEY, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body); // Testing
+    var response = JSON.parse(body);
+    // console.log(response.results[0]);
+    var eventArray = response.results.map(function(el){
+        var anEvent = {};
+        anEvent.name = el.name;
+        anEvent.description = el.description;
+        anEvent.start = el.time;
+        anEvent.end = el.time + el.duration;
+        if (el.venue){
+          anEvent.venue = el.venue.name || null;
+          anEvent.city = el.venue.city|| null;
+          anEvent.address = el.venue.address_1 || null;
+        }
+        return  anEvent;
+    });
+    console.log(eventArray[0]);
   }
   });
 });
