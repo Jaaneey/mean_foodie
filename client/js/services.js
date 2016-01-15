22a0400b7a208d01e4399e7ae9ed65b6a7ebc13a
Janey <janey.keig@gmail.com>
2016-01-15 10:48:25 -0800

    more services

--- client/js/services.js
+++ client/js/services.js
@@ -1,9 +1,19 @@
 app.service("EventService", function($http){
   return {
-    getEvents: function(){},
-    addEvent: function(){},
-    editEvent: function(){},
-    getEvent: function(){},
-    deleteEvent: function(){}
+    getEvents: function(){
+      return $http.get('/api/events');
+    },
+    addEvent: function(event){
+      return $http.post('/api/events', event);
+    },
+    editEvent: function(event){
+      return $http.put('/api/events/' + event._id, event);
+    },
+    getEvent: function(id){
+      return $http.get('/api/events/'+ id);
+    },
+    deleteEvent: function(id){
+      return $http.delete('/api/events/'+ id);
+    }
   };
 });
