579dc1008befae213a6f07aa9c00e3b35fd82acb
Janey <janey.keig@gmail.com>
2016-01-15 10:52:45 -0800

    client route get w/ service

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,15 +1,20 @@
-app.controller("tester", function($scope){
+app.controller("tester", function($scope, EventService){
  $scope.message = "Working";
 });
 
-app.controller("EventsController", function($scope){
+app.controller("EventsController", function($scope, EventService){
   $scope.message = "Events";
+  EventService.getEvents().then(function(){
+    $scope.events = events.data;
+  }).catch(function(err){
+    $scope.errors = err;
+  });
 });
 
-app.controller("NewEventsController", function($scope){
+app.controller("NewEventsController", function($scope, EventService){
  $scope.message = "New";
 });
 
-app.controller("EditEventsController", function($scope){
+app.controller("EditEventsController", function($scope, EventService){
 $scope.message = "Edit";
 });
