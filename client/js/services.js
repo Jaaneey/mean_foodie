181e1ebd9b079b264690b71dc8bef610d4424b5d
Janey <janey.keig@gmail.com>
2016-01-15 10:20:24 -0800

    added event service

---
+++ client/js/services.js
@@ -0,0 +1,9 @@
+app.service("EventService", function($http){
+  return {
+    getEvents: function(){},
+    addEvent: function(){},
+    editEvent: function(){},
+    getEvent: function(){},
+    deleteEvent: function(){}
+  };
+});
