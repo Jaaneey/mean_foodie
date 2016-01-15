83125cc297bd170131b5ce099a03e209d6fba758
Janey <janey.keig@gmail.com>
2016-01-15 10:07:19 -0800

    added event resource routes

--- server/routes/events.js
+++ server/routes/events.js
@@ -6,12 +6,41 @@ db = require('../models');
 // ROUTES
 
 //get all events
+router.get('/', function(req,res){
+  db.Event.find({}, function(err, events){
+    res.status(200).send(events);
+  });
+});
 
 //post new event
 
+router.post('/', function(req,res){
+  db.Event.create(req.body, function(err,event){
+    res.status(201).send(event);
+  });
+});
+
+
+//FOR EDITING EVENTS
 //get single event
+router.get('/:id', function(req, res){
+  db.Event.findById(req.params.id, function(err, event){
+    res.status(200).send(todo);
+  });
+});
+
 //put update single event
+router.put('/:id', function(req,res){
+  db.Event.findByIDAndUpdate(req.params.id, req.body, function(err, event){
+    res.status(200).send(todo);
+  });
+});
 //delete single event
+router.delete('/:id', function(req,res){
+  db.Event.findByIdAndRemove(req.params.id, function(err, event){
+    res.status(200).send(event);
+  });
+});
 
 
 module.exports = router;
--- server/routes/externalApis.js
+++ server/routes/externalApis.js
@@ -2,15 +2,15 @@ var request = require('request'),
 express = require('express'),
 router = express.Router(),
 db = require('../models');
-
+require("locus");
 
 router.get('/meetup', function(req,res){
   // console.log(process.env.MEETUP_KEY);
-  // console.log('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUP_KEY);
+  //console.log('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUP_KEY);
   request('https://api.meetup.com/2/open_events.json?category=10&zip=94101&time=,1w&key='+ process.env.MEETUP_KEY, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var responseM = JSON.parse(body);
-    console.log(responseM.results[0]);
+    // console.log(responseM.results[0]);
     var eventArrayM = responseM.results.map(function(el){
         var anEvent = {};
         anEvent.name = el.name;
@@ -31,15 +31,22 @@ router.get('/meetup', function(req,res){
   });
 });
 
+// EVENTBRITE //////
 
 router.get('/eventbrite', function(req,res){
-  request('https://www.eventbriteapi.com/v3/events/search/?categories=110&venue.city=san-francisco&token='+ process.env.EVENTBRITE_KEY, function (error, response, body) {
+  console.log("wat???");
+  request.get("https://www.eventbriteapi.com/v3/events/search/?categories=110&venue.city=san-francisco"+"&start_date.range_start=2016-01-13T02%3A11%3A30Z&start_date.range_end=2016-01-14T02%3A11%3A30Z"+"&token="+ process.env.EVENTBRITE_KEY, function (error, response, body) {
+    // eval(locus);
+    // console.log(body);
+    // console.log("errors?", error);
+    // console.log("finished first one");
     if (!error && response.statusCode == 200) {
-      // console.log(body);  Testing
       var responseE = JSON.parse(body);
       // console.log(responseE.events[0]);
-      var eventArrayE = responseE.events.map(function(el){
-          var anEvent = {};
+      // var dummy= 0;
+      //****MAP CREATING NEW EVENT ARRAY
+      eventArrayE = responseE.events.map(function(el, idx){
+        var anEvent = {};
           anEvent.name = el.name.text;
           anEvent.url = el.vanity_url;
           anEvent.description = el.description.text;
@@ -49,34 +56,41 @@ router.get('/eventbrite', function(req,res){
           anEvent.venue_id = el.venue_id;
 
           //GRAB VENUE FOR EVENTBRITE 
-          var dummy= 0;
-          if (el.venue_id && dummy < 1){
-            request('https://www.eventbriteapi.com/v3/venues/'+el.venue_id+'/?token='+ process.env.EVENTBRITE_VENUE, function (error, response, body) {
-              if (!error && response.statusCode == 200) {
-                console.log("*****EVENT", anEvent);
-                var responseV = JSON.parse(body);
-                console.log("venue info", body);
-                anEvent.venue = responseV.name;
-                anEvent.city = responseV.address.city;
-                anEvent.address = responseV.address.address_1;
+        
+        // console.log('before venue call',dummy);
+        if (el.venue_id){
+            // dummy ++;
+            // console.log('getting into the loop');
+            // console.log('request endpoint:', 'https://www.eventbriteapi.com/v3/venues/'+el.venue_id+'/?token='+ process.env.EVENTBRITE_VENUE);
+            // console.log(dummy);
+            request('https://www.eventbriteapi.com/v3/venues/el.venue_id/?token='+ process.env.EVENTBRITE_VENUE, function (error, response, body) {
+                // console.log("making the call");
+                if (!error && response.statusCode == 200) {
+                  console.log("*****EVENT", anEvent);
+                  var responseV = JSON.parse(body);
+                  // console.log('venue info', responseV);
+                
+                  anEvent.venue = responseV.name;
+                  anEvent.city = responseV.address.city;
+                  anEvent.address = responseV.address.address_1;
+                  // console.log('event with  new thigs', anEvent);
+                  return anEvent;
+              } else {
+                console.log('venue:'+ el.venue_id, error);
               }
-          });
-          dummy ++;
-          // console.log(dummy);
-            
-       }
+            });
+          } else { return anEvent;}
           
-          return  anEvent;
-      });
-      var tester = eventArrayE[0];
-      console.log(tester);
-      
-      // console.log("test object", tester);
-      // console.log("the ID", tester.venue_id);
-
-    }
-  });
-});
+          
+      }); //**** END OF MAP ITERATOR
+      console.log(eventArrayE);
+      res.send(eventArrayE);
+    } 
+    else {
+        console.log('first call', error);
+      }
+  }); // END API CALL
+}); //END ROUTE
 
 
 //https://www.eventbriteapi.com/v3/venues/12155401/?token=
